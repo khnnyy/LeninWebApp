e3bf8dd85d775e1c1031d9cd6c33b53f8b2d11fe
predator 300 <predator 300@DESKTOP-HEOQC8H>
2024-08-01 16:08:53 +0800

    -removed unnecessary commented codes

--- src/main/java/com/mycompany/webserverlenin/HomeController.java
+++ src/main/java/com/mycompany/webserverlenin/HomeController.java
@@ -1,33 +1,7 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package com.mycompany.webserverlenin;
-//
-//import java.util.List;
-//import org.bson.Document;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-///**
-// *
-// * @author khanny
-// */
-//
-//@Controller
-//@RequestMapping("/")
-//public class HomeController {
-//    
-//    @GetMapping("/home")
-//    public String home() {
-//        return "home";
-//    }
-//    
-//}
-
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.webserverlenin;
 
 import org.springframework.beans.factory.annotation.Autowired;
